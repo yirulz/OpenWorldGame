1c776316e909544a19670f4e8f958cd0f98427d3
Yi <yirulz@hotmail.com>
2019-05-09 10:54:33 +1000

    pads

---
+++ Assets/Scripts/Events/OnTriggerEvent.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+
+public class OnTriggerEvent : MonoBehaviour
+{
+    public string hitTag;
+    public UnityEvent onEnter, onStay, onExit;
+
+
+    private void Reset()
+    {
+        Collider col = GetComponent<Collider>();
+        if(col)
+        {
+            col.isTrigger = true;
+        }
+        else
+        {
+            Debug.LogWarning("The GameObject named " + name + " does not have a colliderm abd will not work");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == hitTag || hitTag == "")
+        {
+            onEnter.Invoke();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == hitTag || hitTag == "")
+        {
+            onStay.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == hitTag || hitTag == "")
+        {
+            onExit.Invoke();
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,22 +8,27 @@ public class Player : MonoBehaviour
     public float walkSpeed = 6f;
     public float gravity = -10f;
     public float jumpHeight = 15f;
-    public float groundRayDistance = 1.1f;
+
+    [Header("Dash")]
+    public float dashSpeed = 50f;
+    public float dashDuration = .5f;
 
     private CharacterController controller; // Reference to CharacterController
     private Vector3 motion; // Is the movement offset per frame
+    private bool isJumping;
+    private float currentJumpHeight, currentSpeed;
 
-    void OnDrawGizmos()
+    void OnValidate()
     {
-        Ray groundRay = new Ray(transform.position, -transform.up);
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, transform.position - transform.up * groundRayDistance);
+        currentJumpHeight = jumpHeight;
+        currentSpeed = walkSpeed;
     }
-
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        currentSpeed = walkSpeed;
+        currentJumpHeight = jumpHeight;
     }
 
     // Update is called once per frame
@@ -32,36 +37,48 @@ public class Player : MonoBehaviour
         // Get W, A, S, D or Left, Right, Up, Down Input
         float inputH = Input.GetAxis("Horizontal");
         float inputV = Input.GetAxis("Vertical");
+
+        bool inputRun = Input.GetKeyDown(KeyCode.LeftShift);
+        bool inputWalk = Input.GetKeyUp(KeyCode.LeftShift);
+
+        //Space bar input
+        bool inputJump = Input.GetButtonDown("Jump");
+
+        //change current speed to runspeed
+        if(inputRun)
+        {
+            currentSpeed = runSpeed;
+        }
+        //change currentspeed to walkspeed
+        if(inputWalk)
+        {
+            currentSpeed = walkSpeed;
+        }
         // Move character motion with inputs
         Move(inputH, inputV);
+
         // Is the Player grounded?
-        if (IsGrounded())
+        if (controller.isGrounded)
         {
             // Cancel gravity
             motion.y = 0f;
             // Pressing Jump Button?
-            if (Input.GetButtonDown("Jump"))
+            if (inputJump)
+            {
+                Jump(jumpHeight);
+            }
+            if(isJumping)
             {
                 // Make the Player Jump!
-                motion.y = jumpHeight;
+                motion.y = currentJumpHeight;
+                //Reset back to false
+                isJumping = false;
             }
         }
         // Apply gravity
         motion.y += gravity * Time.deltaTime;
         // Move the controller with motion
         controller.Move(motion * Time.deltaTime);
-    }
-
-
-    // Check if the player is touching the ground
-    bool IsGrounded()
-    {
-        return controller.isGrounded;
-        //
-        //// Raycast below the player
-        //Ray groundRay = new Ray(transform.position, -transform.up);
-        //// If hitting something
-        //return Physics.Raycast(groundRay, groundRayDistance);
     }
 
     // Move the character's motion in direction of input
@@ -73,8 +90,37 @@ public class Player : MonoBehaviour
         // Convert local space to world space direction
         direction = transform.TransformDirection(direction);
 
+        //Check if direction exceeds magnitude of 1
+        if(direction.magnitude > 1f)
+        {
+            //Normalize it
+            direction.Normalize();
+        }
+
         // Apply motion to only X and Z
-        motion.x = direction.x * walkSpeed;
-        motion.z = direction.z * walkSpeed;
+        motion.x = direction.x * currentSpeed;
+        motion.z = direction.z * currentSpeed;
     }
+
+
+    public void Jump(float height)
+    {
+        isJumping = true; //Tell controller to jump at the right time
+        currentJumpHeight = height;
+    }
+
+    public IEnumerator SpeedBoost(float startSpeed, float endSpeed, float duration)
+    {
+        currentSpeed = startSpeed;
+
+        yield return new WaitForSeconds(duration);
+
+        currentSpeed = endSpeed;
+    }
+
+    public void Dash()
+    {
+        StartCoroutine(SpeedBoost(dashSpeed, walkSpeed, dashDuration));
+    }
+
 }
